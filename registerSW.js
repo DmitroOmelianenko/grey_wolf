b9c9bec636785818218bd8b75ac5085404506798
DmitroOmelianenko <DmitroOmelianenko@users.noreply.github.com>
2025-11-16 20:18:38 +0000

    deploy: 95662e1d1d6f0a3b9b21ccdf2865a0e4a93460a6

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/grey_wolf/sw.js', { scope: '/grey_wolf/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
